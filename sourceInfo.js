827bc9f8205accd702e63068b570b5f12299347d
Lenard Frommelt <lenny09918050@gmail.com>
2024-11-30 05:18:19 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,9 +1,13 @@
 module.exports = {
     getDependencies: () => {
         return {
+            "cached-persistentstate": "^0.0.1",
             "key-block": "^0.0.2",
-            "secret-manager-crypto-utils": "^0.2.0",
-    	    "thingy-sci-base":"^0.0.8"
+            "secret-manager-crypto-utils": "^0.3.0",
+            "sanitize-html": "^2.13.1",
+            "thingy-crypto-node": "^0.0.1",
+    	    "thingy-sci-base":"^0.0.10",
+            "validatabletimestamp": "^0.0.1"
         }
     }
 }
